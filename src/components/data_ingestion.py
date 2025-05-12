5d661d592eb60032a3d6434ae5c5c81934171ce4
Chitransh <singhalchitransh01@gmail.com>
2025-05-12 12:08:03 +0530

    Performed parameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+# from src.components.data_transformation import DataTransformation
+# from src.components.data_transformation import DataTransformationConfig
+
+# from src.components.model_trainer import ModelTrainer
+# from src.components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -44,9 +47,12 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys) from e
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
+# if __name__ == "__main__":
+#     obj = DataIngestion()
+#     train_data,test_data = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    preprocessor = data_transformation.initiate_data_transformation(train_data, test_data)
+#     data_transformation = DataTransformation()
+#     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+#     model_trainer = ModelTrainer()
+#     print(model_trainer.initiate_model_trainer(train_arr, test_arr))
